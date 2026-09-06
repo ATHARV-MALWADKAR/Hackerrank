f5ea2ae280599538a801241af37ca47001d3b2a6
Atharv <atharvmalwadkar17@gmail.com>
2026-09-06 23:05:39 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 13579-arrays-ds-Hackerrank/13579-arrays-ds.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    a_len = len(a)
+    a_half_len = a_len / 2
+    i,j = 0, a_len -1
+    
+while i < a_half_len:
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+    i += 1
+    j -= 1
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
