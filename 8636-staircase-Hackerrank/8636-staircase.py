fea85a439804219d9a9a1e728dc79f9f1c556112
Atharv <atharvmalwadkar17@gmail.com>
2026-09-22 19:59:55 +0530

    Force committed solution - Time: O(n^2), Space: O(n)

---
+++ 8636-staircase-Hackerrank/8636-staircase.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "#" * i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
